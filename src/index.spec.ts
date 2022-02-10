92733a04f8b9c3b44d0f33e2d6d4cd21b8a0992f
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2022-02-10 20:06:15 +0100

    Gist

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,15 @@
-it("That's a test!", function () {
-  expect(1 + 1).toEqual(2);
+type RandomGenerator = () => number;
+
+export function random(min: number, max: number, generator: RandomGenerator): number {
+  return min + (max - min) * generator();
+}
+
+test("The lowest value possible should be 'min'", function () {
+  const generator = () => 0;
+  expect(random(1, 10, generator)).toEqual(1);
+});
+
+test("The highest value possible should be 'max'", function () {
+  const generator = () => 1;
+  expect(random(1, 10, generator)).toEqual(10);
 });
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { random } from "./index.spec";
+
+// Usage in production
+const n = random(1, 10, Math.random);
